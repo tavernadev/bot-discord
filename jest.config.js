62d04e1928ed5ed91dbb4299aeabcc2371189fd1
bbruno001 <bruno_developer@outlook.com>
2020-12-09 15:01:58 -0300

    feat: add codecov

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  coverageReporters: ['json', 'text'],
+  coverageReporters: ['lcov', 'json', 'text'],
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.spec.js']
 }
